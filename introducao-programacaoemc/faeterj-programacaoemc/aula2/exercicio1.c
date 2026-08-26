3c0714633f26a58d66c101fd24bcd0c2382e2fd5
unknown <tvlisboa@hotmail.com>
2026-08-26 10:53:54 -0300

    exercicio-1-aula2

--- introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio1.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio1.c
@@ -2,3 +2,25 @@
  * Seu peso e mostre na telas informacoes
  * Nome do usuario e o peso informado
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    char name[100];
+    double peso;
+
+    printf("Insira seu nome a seguir : ");
+    scanf("%s", name);
+
+    printf("Insira seu peso a seguir : ");
+    scanf("%lf", &peso);
+
+    printf("Dados informados pelo usuario.\n");
+    printf("Nome informado do usuario: %s\n", name);
+    printf("Peso informado do usuario: %.2lf", peso);
+    printf("\nFim do algoritmo! \n");
+    return 0;
+ }
