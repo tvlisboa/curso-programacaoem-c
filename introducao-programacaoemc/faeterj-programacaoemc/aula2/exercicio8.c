f7fe729216ab273fb4a6035660fef3e9020f7b72
unknown <tvlisboa@hotmail.com>
2026-08-26 17:16:59 -0300

    exercicio8-aula2

--- introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio8.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio8.c
@@ -10,7 +10,27 @@
 
  int main(int argc, char const *argv[])
  {
-    /* code */
+    double n1;
+    double n2;
+
+    printf("************************ \n");
+    printf("* PRODUTO DE 2 VALORES * \n");
+    printf("************************ \n");
+
+    printf("Informe 2 numeros a seguir \n");
+    printf("Numero 1: ");
+    scanf("%lf", &n1);
+
+    printf("Numero 2: ");
+    scanf("%lf", &n2);
+
+    double mult = (n1 * n2);
+
+    printf("* Numeros informados pelo usuario *");
+    printf("* \nNumero 1: %.2f", n1);
+    printf("* \nNumero 2: %.2f", n2);
+    printf("* \nResultado do produto dos numeros informados: %.2f", mult);
+    system("\npause");
     return 0;
  }
  
