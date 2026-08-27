d9a6dde65a355453d562ff8ee87f18eee7a33096
unknown <tvlisboa@hotmail.com>
2026-08-27 12:50:46 -0300

    exercicio10-aula2

--- introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio10.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio10.c
@@ -4,3 +4,33 @@
  * Nome do funcionario - seu salario atual
  * Em seguida - mostre na tela o nome do funcionario e seu salario reajustado em 10%
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char nome[];
+double salario;
+double novosalario;
+
+
+int main(int argc, char const *argv[])
+{
+    printf("************************************************* \n");
+    printf("*         SISTEMA DE CALCULO SALARIAL           * \n");
+    printf("************************************************* \n\n");
+    printf("* INSIRA AS INFORMACOES DO FUNCIONARIO A SEGUIR * \n");
+
+    printf("Nome: ");
+    scanf("%s", nome);
+
+    printf("Salario: ");
+    scanf("%lf", &salario);
+
+    novosalario = salario+(salario*10)/100;
+
+    printf("Dados informados do funcionario \n");
+    printf("Nome funcionario: %s", nome);
+    printf("\nSalario anterior: %.2lf", salario);
+    printf("\nNovo salario: %.2lf", novosalario);
+    return 0;
+}
