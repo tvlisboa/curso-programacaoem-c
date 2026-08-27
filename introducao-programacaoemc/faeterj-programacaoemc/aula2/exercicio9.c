7a5aceaacebc08d8881922ce738d5edc8cce9793
tvlisboa <tvlisboa@hotmail.com>
2026-08-26 22:11:14 -0300

    exercicio9-aula2

--- introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio9.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio9.c
@@ -6,3 +6,20 @@
  * Valor unitario do produto
  * Calcule o valor total da compra e mostre na tela
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[]) {
+
+    char produto[100];
+    int quantidade;
+    double preco;
+
+    printf("************************ \n");
+    printf("* CONSULTA DE PRODUTOS * \n");
+    printf("************************ \n");
+
+    return 0;
+}
+ 
