3b20c2b2504e6d5cbcd43e6bb3c46b1bfc428f78
tvlisboa <tvlisboa@hotmail.com>
2026-08-30 13:22:02 -0300

    exercicio12-aula2

--- introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio12.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio12.c
@@ -26,13 +26,19 @@
    printf("Informe a temperatura local em Celsius: ");
    scanf("%lf", &temperaturaLocal);
 
-   double temperaturaFh = (temperaturaLocal * 9/5) + 32;  //conversao para celsius
+   double temperaturaFh = (temperaturaLocal * 9/5) + 32;                        //conversao para celsius
+   double temperaturaReamur = (temperaturaLocal) * 0.8;                         //conversao para reaumur
+   double temperaturaKelvin=(temperaturaLocal) + 273.15;                        //conversao para kelvin
+   double temperaturaRankine=(temperaturaLocal * 95) + 491.67;                  //conversao para rankine
 
    printf("********************************* \n");
    printf("\n Dados informados pelo usuario: ");
-   printf("\n Cidade: %s", nomeCidade);
+   printf("\n Cidade local: %s", nomeCidade);
    printf("\n Temperatura local em C: %.2lf", temperaturaLocal);
    printf("\n Temperatura local em F: %.2lf", temperaturaFh);
+   printf("\n Temperatura local em Reamur: %.2lf", temperaturaReamur);
+   printf("\n Temperatura local em Kelvin: %.2lf", temperaturaKelvin);
+   printf("\n Temperatura local em Rankine: %.2lf", temperaturaRankine);
    system("pause");
    return 0;
  }
