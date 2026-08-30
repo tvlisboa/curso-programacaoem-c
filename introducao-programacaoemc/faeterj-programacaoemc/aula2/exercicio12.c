eca91029ee0bb331ecec53e04909a7d0f825e173
tvlisboa <tvlisboa@hotmail.com>
2026-08-29 23:04:23 -0300

    exercicio12-aula2

--- introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio12.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio12.c
@@ -2,7 +2,8 @@
  * Conversao de temperaturas
  * Faca um algoritmo que leia nome da cidade
  * E sua temperatura em celsius
- * Converta para fahrenheit e mostre na tela as informacoes
+ * Converta para fahrenheit - reaumur - Kelvin e Rnakine
+ * Mostre na tela as informacoes
  * fh = (celsius * 9/5) + 32
  */
 
@@ -11,8 +12,29 @@
 
  int main(int argc, char const *argv[])
  {
-    /* code */
-    return 0;
+
+   char nomeCidade[100];
+   double temperaturaLocal;
+
+   printf("***************************** \n");
+   printf("* CONVERSÕES DE TEMPERATURAS * \n");
+   printf("***************************** \n");
+
+   printf("Informe o nome da cidade abaixo: ");
+   scanf("%s", nomeCidade);
+
+   printf("Informe a temperatura local em Celsius: ");
+   scanf("%lf", &temperaturaLocal);
+
+   double temperaturaFh = (temperaturaLocal * 9/5) + 32;  //conversao para celsius
+
+   printf("********************************* \n");
+   printf("\n Dados informados pelo usuario: ");
+   printf("\n Cidade: %s", nomeCidade);
+   printf("\n Temperatura local em C: %.2lf", temperaturaLocal);
+   printf("\n Temperatura local em F: %.2lf", temperaturaFh);
+   system("pause");
+   return 0;
  }
  
 
---
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio16.c
@@ -0,0 +1,13 @@
+/*
+ * Crie um algoritmo para solicitar ao usuario
+ * Nome da cidade e sua temperatura local
+ * Solicite para informar se é em celsius ou fahrenheit
+ * Faca suas conversoes utilizando o switch-case 
+ * e validacoes
+ * Acima de 15 graus - clima agradavel
+ * Acima de 25 graus - clima quente
+ * Entre 10 e 15 graus - clima frio mas tranquilo
+ * Entre 0 e 15 - clima realmente frio
+ * Abaixo de 0 - frio extremo - pretecao
+ * Mostre na tela as informacoes uteis
+ */
