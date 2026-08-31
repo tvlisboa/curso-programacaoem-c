b1c906a5a8aab02ec9f5e927455a483b34f0bc1f
unknown <tvlisboa@hotmail.com>
2026-08-31 16:53:10 -0300

    exercicio14-aula2

--- introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio14.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio14.c
@@ -11,8 +11,8 @@
  #include <stdio.h>
  #include <stdlib.h>
 
- //char marca[50]
- //char modelo[100]
+ //char marca[50]            - usando switch-case colocar 5 marcas distintas para o usuario escolher - [1] - BYD / [2] - FORD /[3] - VW / [4] - FIAT / [5] - CHEVROLET               
+ //char modelo[100]          - usando switch-case colocar dentro de cada marca - 5 modelos para usuario escolher e 1 opcao inserir novo veiculo - 0 sair / 9 mais informacoes
  //int ano de fabricacao
  //double cilindradas
  //char tipoCombustivel[100] - usando switchcase [1] - gasolina / [2] - alcool / [3] - flex / [4] - gas / [5] - hibrido / [6] - eletrico
@@ -27,13 +27,17 @@
 
     char marcaVeiculo[50];
     char modeloVeiculo[50];
-    char combustivel[50];
+    char tipoDeCombustivel[50];
     int anoFabricacao;
     double cilindradas;
+    double quilometrosRodados;
+    double litrosCombustivel;
+    double valorCombustivel;
 
     printf("************************************** \n");
     printf("*     SISTEMA DE COSUMO VEICULAR     * \n");
     printf("*   INSIRA AS INFORMACOES A SEGUIR:  * \n");
+    printf("* DADOS DO VEICULO * \n");
     printf("* Marca do veiculo: ");
     scanf("%s", marcaVeiculo);
 
@@ -47,7 +51,17 @@
     scanf("%lf", &cilindradas);
 
     printf("* Combustivel usado");
-    scanf("%s", combustivel);
+    scanf("%s", tipoDeCombustivel);
+
+    printf("* DADOS DA VIAGEM * \n");
+    printf("* Quilometros rodados: ");
+    scanf("%lf",quilometrosRodados);
+
+    printf("* Valor do combustivel: *");
+    scanf("%lf", valorCombustivel);
+
+    double consumoMedio = (quilometrosRodados / litrosCombustivel);              //calculo de consumo médio
+    double gastoTotal = 
 
     /* teste de saida de dados informados*/
     printf("**************************************");
