d2f98be555d831beff78af759c3e87626d5e5aef
unknown <tvlisboa@hotmail.com>
2026-09-01 11:53:02 -0300

    exercicio15-aula2

--- introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio15.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio15.c
@@ -16,12 +16,14 @@
 
  #include <stdio.h>
  #include <stdlib.h>
+ #include <string.h>
 
 
  int main(int argc, char const *argv[])
  {
 
     char nomeUsuario[100];
+    char mensagem[100];
     int idade;
     double altura , peso;
 
@@ -36,13 +38,38 @@
     printf("* Idade do usuario: ");
     scanf("%d", &idade);
 
+    while (idade<0)
+    {
+        printf("* Dados informados estão incorretos - por favor tente novamente! \n");
+        printf("* Idade do usuario: ");
+        scanf("%d", &idade);
+    }
+    
+
     printf("* Altura do usuario: ");
     scanf("%lf", &altura);
 
     printf("* Peso do usuario: ");
     scanf("%lf", &peso);
 
-    /* teste saida de dados do usuario */
+    double calculoImc = peso / (altura * altura);
+
+    /* validacao imc e suas taxas*/
+    if(calculoImc>40){
+        strcpy(mensagem, "Classificação de Obesidade Grau III");
+    }else if(calculoImc>=35){
+        strcpy(mensagem, "Classificação de Obesidade Grau II");
+    }else if(calculoImc>=30){
+        strcpy(mensagem, "Classificação de Obesidade Grau I");
+    }else if(calculoImc>=25){
+        strcpy(mensagem, "Classificação acima do peso ideal");
+    }else if(calculoImc>=18.5){
+        strcpy(mensagem, "Classificação de peso ideal");
+    }else{
+        strcpy(mensagem, "Classificação abaixo do peso ideal");
+    }
+
+    printf("**************************************** \n");
     printf("* Nome do usuario:  %s", nomeUsuario);
     printf("\n");
     printf("* Idade do usuario: %d", idade);
@@ -51,6 +78,12 @@
     printf("\n");
     printf("* Peso do usuario: %.2lf", peso);
     printf("\n");
+    printf("* Calculo IMC: %.2lf", calculoImc);
+    printf("\n");
+    printf("* Faixa de peso do usuario: %s*" , mensagem);
+    printf("\n");
+    printf("**************************************** \n");
+    printf("* Fim do algoritmo * \n");
     system("pause");
     return 0;
  }
