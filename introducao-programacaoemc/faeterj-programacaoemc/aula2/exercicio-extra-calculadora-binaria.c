53984cf9b91eb81527035d0c84b1208f2636fe26
tvlisboa <tvlisboa@hotmail.com>
2026-09-01 21:08:06 -0300

    exercicio-extra-calculadora-binaria-aula2

---
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio-extra-calculadora-binaria.c
@@ -0,0 +1,17 @@
+/*
+* Desenvolva um programa em linguagem C que funcione 
+* como uma calculadora de conversão entre sistemas numéricos, 
+* utilizando os conhecimentos da disciplina de OC1 
+* (Organização de Computadores) e os conteúdos estudados em 
+* Programação em C.
+* O programa deverá permitir que o usuário escolha, 
+* através de um menu, qual conversão deseja realizar.
+* O programa devera apresentar incialmente o seguinte menu ao usuario
+* [1] - Conversao de decimal para binario
+* [2] - Binário para decimal
+* [3] - Decimal para octal
+* [4] - Octal para decimal
+* [5] - Decimal para hexadecimal
+* [6] - Hexadecimal para decimal
+* [0] - Sair da tela
+*/
