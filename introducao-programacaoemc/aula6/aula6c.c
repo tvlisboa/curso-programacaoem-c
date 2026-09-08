6db7f0193993d6a5e49135b2ae95a5f1b24b1fd1
tvlisboa <tvlisboa@hotmail.com>
2026-09-08 20:57:04 -0300

    aula6-cursolingaugemc-exercicio-switch-case-menu-callcenter

---
+++ introducao-programacaoemc/aula6/aula6c.c
@@ -0,0 +1,24 @@
+/*
+ * Crie um algoritmo em c que utilize a estrutura switch - case 
+ * para criar um menu de call center
+ * utilize de 1 a 5 e tambem 9 e 0 para as seguintes opcoes
+ * [1] - Financeiro
+ * [2] - Suporte tecnico
+ * [3] - Comercial
+ * [4] - SAC
+ * [5] - Qualidade
+ * [9] - Outros assuntos
+ * [0] - Sair
+ * Utilize a estrutura enquanto - ao apertar 0 sai do menu
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+
+    system("pause");
+    return;
+ }
+ 
