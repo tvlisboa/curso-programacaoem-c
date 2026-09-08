8e5e5d02a4d322dea7552f9df7be60ce018a9793
tvlisboa <tvlisboa@hotmail.com>
2026-09-08 20:07:57 -0300

    aula5-cursobasicoc-exercicio-apto-inapto-dirigir

---
+++ introducao-programacaoemc/aula5/aula5b.c
@@ -0,0 +1,8 @@
+/*
+* Crie um algoritmo onde verifique a idade do usuario
+* Solicite o nome do usuario
+* ano de nascimento - ano atual
+* calcule sua idade e mostre na tela
+* apto e inapto a dirigir
+* nao aceite ano de nascimento maior que o ano atual
+*/
