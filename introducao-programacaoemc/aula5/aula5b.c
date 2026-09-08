858ef068220fe3f81120ecf8339e7f1053bcef93
tvlisboa <tvlisboa@hotmail.com>
2026-09-08 20:35:21 -0300

    aula5-cursobasicoc-exercicio-apto-inapto-dirigir-conversaouppercase-validacao-idade

--- introducao-programacaoemc/aula5/aula5b.c
+++ introducao-programacaoemc/aula5/aula5b.c
@@ -9,14 +9,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, char const *argv[])
 {
-    char name[100];
+    char nomeMotorista[100];
     char placaCarro[9];
     int anoNascimento;
     int anoAtual;
-    int idadeUsuario;
+    int idadeMOtorista;
 
     /* entrada de dados do usuario */
     printf("********************************* \n");
@@ -26,7 +27,7 @@ int main(int argc, char const *argv[])
     
     printf("* Insira as informações do motorista a seguir * \n");
     printf("* Nome do motorista: ");
-    scanf("%s", name);
+    scanf("%s", nomeMotorista);
 
     printf("Ano de nascimento do motorista: ");
     scanf("%d", &anoNascimento);
@@ -34,6 +35,7 @@ int main(int argc, char const *argv[])
     printf("Ano atual: ");
     scanf("%d", &anoAtual);
 
+    /* nao aceita dados se o ano nascimento for maior que o ano atual*/
     while (anoNascimento >= anoAtual)
     {
         printf("Os dados informados estao incorretos - tente novamente \n");
@@ -44,16 +46,27 @@ int main(int argc, char const *argv[])
         printf("Ano atual: ");
         scanf("%d", &anoAtual);
     }
+
+    idadeMOtorista = (anoAtual - anoNascimento);
+
+    if(idadeMOtorista>=18){
+        printf("Usuario informado - está apto a dirigir \n");
+    }else{
+        printf("Usuario informado - não está apto a dirigir \n");
+    }
     
-
-    
-
-
-
+    /* conversao uppercase */
+    for(int i = 0; nomeMotorista[i]!=0; i++){
+        nomeMotorista[i] = toupper(nomeMotorista[i]);
+    }
+  
     /* teste de saida de dados do usuario */
-    printf("Nome do motorista: %s\n" , name);
+    printf("********************************* \n");
+    printf("Nome do motorista: %s\n" , nomeMotorista);
     printf("Ano de nascimento: %d\n", anoNascimento);
     printf("Ano atual: %d\n", anoAtual);
+    printf("Idade do motorista: %d\n " , idadeMOtorista);
+    printf("********************************* \n");
     system("pause");
     return 0;
 }
