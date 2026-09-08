2abc9263baf3127c55d532c4d3f4386162c37e0f
tvlisboa <tvlisboa@hotmail.com>
2026-09-08 20:25:45 -0300

    aula5-cursobasicoc-exercicio5-apito-dirigir

--- introducao-programacaoemc/aula5/aula5a.c
+++ introducao-programacaoemc/aula5/aula5a.c
@@ -13,7 +13,7 @@
 
 /* opcoes como - printf and scanf */
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
--- introducao-programacaoemc/aula5/aula5b.c
+++ introducao-programacaoemc/aula5/aula5b.c
@@ -6,3 +6,54 @@
 * apto e inapto a dirigir
 * nao aceite ano de nascimento maior que o ano atual
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    char name[100];
+    char placaCarro[9];
+    int anoNascimento;
+    int anoAtual;
+    int idadeUsuario;
+
+    /* entrada de dados do usuario */
+    printf("********************************* \n");
+    printf("* SISTEMA DE TRANSITO UNIFICADO * \n");
+    printf("********************************* \n");
+    printf("\n");
+    
+    printf("* Insira as informações do motorista a seguir * \n");
+    printf("* Nome do motorista: ");
+    scanf("%s", name);
+
+    printf("Ano de nascimento do motorista: ");
+    scanf("%d", &anoNascimento);
+
+    printf("Ano atual: ");
+    scanf("%d", &anoAtual);
+
+    while (anoNascimento >= anoAtual)
+    {
+        printf("Os dados informados estao incorretos - tente novamente \n");
+        
+        printf("Ano de nascimento do motorista: ");
+        scanf("%d", &anoNascimento);
+
+        printf("Ano atual: ");
+        scanf("%d", &anoAtual);
+    }
+    
+
+    
+
+
+
+    /* teste de saida de dados do usuario */
+    printf("Nome do motorista: %s\n" , name);
+    printf("Ano de nascimento: %d\n", anoNascimento);
+    printf("Ano atual: %d\n", anoAtual);
+    system("pause");
+    return 0;
+}
