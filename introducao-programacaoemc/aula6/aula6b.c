b11691b9d4f8a0b244ec54b2ac0e65b38aaa0749
unknown <tvlisboa@hotmail.com>
2026-09-09 13:07:19 -0300

    aula6-exercicio-menu-jogos

--- introducao-programacaoemc/aula6/aula6b.c
+++ introducao-programacaoemc/aula6/aula6b.c
@@ -19,43 +19,56 @@ int main(int argc, char const *argv[]){
     printf("************************* \n");
     printf("*     MENU DE JOGOS     * \n");
     printf("* UTILIZE O MENU ABAIXO * \n");
-    printf("* [ 1 ] - LOAD GAME.    * \n");
-    printf("* [ 2 ] - RESUME GAME.  * \n");
-    printf("* [ 3 ] - OPTIONS.      * \n");
-    printf("* [ 4 ] - MAP.          * \n");
-    printf("* [ 5 ] - STATS.        * \n");
-    printf("* [ 6 ] - QUIT GAME.    * \n");
+    printf("* [ 1 ] - NEW GAME.     * \n");
+    printf("* [ 2 ] - LOAD GAME.    * \n");
+    printf("* [ 3 ] - RESUME GAME.  * \n");
+    printf("* [ 4 ] - OPTIONS.      * \n");
+    printf("* [ 5 ] - MAP.          * \n");
+    printf("* [ 6 ] - STATS.        * \n");
+    printf("* [ 9 ] - HOME          * \n");
+    printf("* [ 0 ] - QUIT GAME.    * \n");
     printf("************************* \n");
-    scanf("%d\n", &option);
+    scanf("%d", &option);
 
     switch (option)
     {
+
     case 1:
         system("cls");
-        printf("Loading Game ... \n");
+        printf("New game ... \n");
         break;
 
     case 2:
         system("cls");
-        printf("Resume Game ...\n");
+        printf("Loading Game ...\n");
         break;
 
     case 3:
         system("cls");
-        printf("Options ...\n");
+        printf("Resume  ...\n");
         break;
 
     case 4:
         system("cls");
-        printf("Map ...\n");
+        printf("Options ...\n");
         break;
 
     case 5:
+        system("cls");
+        printf("Map ...\n");
+        break;
+
+    case 6:
         system("cls");
         printf("Stats ...\n");
         break;
 
     case 9:
+        system("cls");
+        printf("Home ...\n");
+        break;
+
+    case 0:
         system("cls");
         printf("Quit Game ...\n");
         break;
