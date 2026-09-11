b7d5e31192f368e4f566a8f2c35a04a8a1639259
unknown <tvlisboa@hotmail.com>
2026-09-11 12:53:55 -0300

    exercicio2-lista1

--- introducao-programacaoemc/faeterj-programacaoemc/aula4/lista-1-exercicios-algoritmos/bloco1/exercicio2.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula4/lista-1-exercicios-algoritmos/bloco1/exercicio2.c
@@ -31,7 +31,71 @@
     printf("* [ 0 ] - SAIR * \n");
     scanf("%d", &option);
 
+    switch (option)
+    {
+
+    case 1:
+        system("cls");
+        printf("***************************************** \n");
+        printf("* [ 1 ] - OPCAO ESCOLHIDA : CONSULTA DE ANIMAIS * \n");
+        printf("* INSIRA AS INFORMACOES DO ANIMAL A SEGUIR: * \n");
+        break;
     
+    case 2:
+        system("cls");
+        printf("***************************************** \n");
+        printf("* [ 2 ] - OPCAO ESCOLHIDA : CADASTRO DE ANIMAIS * \n");
+        printf("* INSIRA AS INFORMACOES DO ANIMAL A SEGUIR: * \n");
+        break;
+    
+    case 3:
+        system("cls");
+        printf("* [ 3 ] - OPCAO ESCOLHIDA : CONSULTA DE ESTOQUE * \n");
+        break;
+    
+    case 4:
+        system("cls");
+        printf("* [ 4 ] - OPCAO ESCOLHIDA : CADASTRO DE PRODUTOS * \n");
+        break;
+
+    case 5:
+        system("cls");
+        printf("* [ 5 ] - OPCAO ESCOLHIDA : ORCAMENTOS * \n");
+        //1 SALVAR ORCAMENTO 
+        //2 EXCUIR ORCAMENTO
+        //3 CANCELAR ORCAMENTO
+        //4 NOVO ORCAMENTO
+        printf("* Numero de animais disponiveis no haras ? ");
+        scanf("%d", &cavalos);
+
+        printf("* Valor da ferradura utilizada: ");
+        scanf("%lf", &valorFerradura);
+
+        printf("* Total de ferraduras utilizadas por animal: ");
+        scanf("%d", &ferradurasAnimal);
+
+        //valor total de ferraduras gastas por animal
+        totalFerraduras = cavalos * ferradurasAnimal;
+        //5 PEQUISAR
+        //9 MAIS OPCOES
+        //0 SAIR
+        break;
+
+    case 9:
+        system("cls");
+        printf("* [ 9 ] - OPCAO ESCOLHIDA : ORCAMENTOS * \n");
+        break;
+
+    case 0:
+        break;
+    
+    default:
+        printf("***************************************** \n");
+        printf("* OPCAO ESCOLHIDA É INVÁLIDA * \n");
+        printf("***************************************** \n");
+        break;
+    }
+
     system("pause");
     return 0;
  }
