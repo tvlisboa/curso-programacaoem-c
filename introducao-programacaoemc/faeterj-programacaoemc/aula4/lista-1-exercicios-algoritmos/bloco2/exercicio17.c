b90f96c82b4be6e5825fb88975d51df202888368
unknown <tvlisboa@hotmail.com>
2026-09-11 16:52:54 -0300

    exercicio17-lista1-temperatura-cidade

--- introducao-programacaoemc/faeterj-programacaoemc/aula4/lista-1-exercicios-algoritmos/bloco2/exercicio17.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula4/lista-1-exercicios-algoritmos/bloco2/exercicio17.c
@@ -19,7 +19,26 @@
     printf("* INSIRA OS DADOS A SEGUIR PARA CONTINUAR * \n");
     printf("******************************************* \n");
 
-    printf("");
+    printf("* Nome da cidade: ");
+    scanf("%s", nomeCidade);
+
+    printf("* Temperatura local em CELSIUS: ");
+    scanf("%lf", &temperaturaLocal);
+
+    //conversao de temperaturas
+    temperaturaFahre = (temperaturaLocal * 1.8) + 32;
+
+    //teste de saida de dados
+    printf("******************************************* \n");
+    printf("*  SISTEMA DE CONVERSAO DE TEMPERATURAS   * \n");
+    printf("*    DADOS  INFORMADOS  PELO  USUARIO     * \n");
+    printf("Cidade informada: %s", nomeCidade);
+    printf("\n");
+    printf("Temperatura local: %.2lf", temperaturaLocal);
+    printf("\n");
+    printf("Temperatura convertida em Fahrenheit: %.3lf", temperaturaFahre);
+    printf("\n");
+    printf("******************************************* \n");
 
     system("pause");
     return 0;
