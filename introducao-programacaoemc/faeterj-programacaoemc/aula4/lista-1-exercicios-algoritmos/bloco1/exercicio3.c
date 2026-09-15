eeb76c914ea44067e39ecc8678460401147a47d9
unknown <tvlisboa@hotmail.com>
2026-09-15 16:37:27 -0300

    exercicio3-padaria-controledegastos

--- introducao-programacaoemc/faeterj-programacaoemc/aula4/lista-1-exercicios-algoritmos/bloco1/exercicio3.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula4/lista-1-exercicios-algoritmos/bloco1/exercicio3.c
@@ -1,8 +1,22 @@
 /*
- *  A padaria Hotpão vende uma certa quantidade de pães franceses e uma quantidade de broas a cada dia.
+ *  A padaria Hotpão vende uma certa quantidade de 
+ *  pães franceses e uma quantidade de broas a cada dia.
  *  Cada pãozinho custa R$ 0,12 e a broa custa R$ 1,50.
  *  Ao final do dia, o dono quer saber quanto arrecadou com a venda dos pães e broas (juntos), 
  *  e quanto deve guardar numa conta de poupança (10% do total arrecadado). 
- *  Você foi contratado para fazer os cálculos para o dono. Com base nestes fatos, faça um algoritmo para ler as quantidades de pães e de broas,
+ *  Você foi contratado para fazer os cálculos para o dono. 
+ *  Com base nestes fatos, faça um algoritmo para ler as quantidades de pães e de broas,
  *  e depois calcular os dados solicitados. 
  */
+
+ // pao frances - 0.12
+ // broinhas - 1.50
+ // vendatotal de pao frances
+ // vendatotal de broinhas
+ // vendageral = valortotal pao frances + valortotal de broinhas    //valor geral no final do dia itens juntos
+ // poupanca = (vendasGeral*10) / 100                               //porcentagem poupanca
+
+ // 1 - Valor do pao frances
+ // 2 - Valor da broinha
+ // 3 - Quantidade de pao frances
+ // 4 - Quantidade de broinha
