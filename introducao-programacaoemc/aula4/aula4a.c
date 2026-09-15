4d6a83f616d9a8de2696dce26f2719929b14f3b1
unknown <tvlisboa@hotmail.com>
2026-09-15 15:28:08 -0300

    exercicio-aula4-cursoc-calcularidade-implementacao-while

--- introducao-programacaoemc/aula4/aula4a.c
+++ introducao-programacaoemc/aula4/aula4a.c
@@ -17,6 +17,39 @@
 
 int main(int argc, char const *argv[])
 {
+    int anoNascimento;
+    int anoAtual;
+    int idade;
     
+    printf("************************************* \n");
+    printf("*   EXERCICIO  CALCULADORA  IDADE   * \n");
+    printf("************************************* \n");
+
+    printf("* INSIRA OS DADOS DO USUARIO ABAIXO * \n");
+    printf("* Ano de nascimento : ");
+    scanf("%d", &anoNascimento);
+
+    printf("* Ano atual : ");
+    scanf("%d", &anoAtual);
+
+    while (anoNascimento>anoAtual) {
+        printf("Dados informados estao incorretos - tente novamente \n");
+
+        printf("* Ano de nascimento : ");
+        scanf("%d", &anoNascimento);
+
+        printf("* Ano atual : ");
+        scanf("%d", &anoAtual);
+    }
+
+    idade = anoAtual - anoNascimento;
+
+    /* teste de saida de dados */
+    printf("Ano de nascimento %d", anoNascimento);
+    printf("\n");
+    printf("Ano atual %d", anoAtual);
+    printf("\n");
+
+    system("pause");
     return 0;
 }
