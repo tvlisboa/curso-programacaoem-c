8fd4c6589047080009520f248eaa2d02cfeca2eb
unknown <tvlisboa@hotmail.com>
2026-09-15 15:13:14 -0300

    exercicio-aula4-cursoc-calcularidade

---
+++ introducao-programacaoemc/aula4/aula4a.c
@@ -0,0 +1,22 @@
+/**
+ * Utilização da variavel idade
+ * Calcule a idade do usuario a seguir
+ * Solicite ao usuario ano de nascimento - ano atual
+ * Calcule sua idade e verifique na tela
+ * Se for maior de 60 anos - idoso
+ * Se for maior de 18 anos - adulto
+ * Se for maior de 16 anos adolescente
+ * Caso ao contrario crianca
+ * Nao aceite ano de nascimento ser maior do ano atual
+ * Mostre as informações na tela
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
