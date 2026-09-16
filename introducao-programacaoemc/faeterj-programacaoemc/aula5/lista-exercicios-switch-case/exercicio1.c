1a0d56ef5e4ae2de943ce51b320301cda258d939
unknown <tvlisboa@hotmail.com>
2026-09-16 08:37:48 -0300

    aula5-exercicios-switch-case

---
+++ introducao-programacaoemc/faeterj-programacaoemc/aula5/lista-exercicios-switch-case/exercicio1.c
@@ -0,0 +1,14 @@
+/**
+ * Faca um programa em C - que leia dois numeros inteiros
+ * Nao aceite numeros inferiores a zero, em seguida peça para o usuario
+ * selecionar uma operacao
+ * 1 - SOMA
+ * 2 - SUBTRACAO
+ * 3 - DIVISAO
+ * 4 - MULTIPLICACAO
+ * 5 - PORCENTAGEM
+ * 6 - POTENCIACAO
+ * 7 - RESTO
+ * 9 - MAIS INFORMACOES
+ * 0 - SAIR
+ */
---
+++ introducao-programacaoemc/faeterj-programacaoemc/aula5/lista-exercicios-switch-case/exercicio2.c
@@ -0,0 +1,21 @@
+/**
+ * Crie um algoritmo para gestao de uma academia e planos com gympass
+ * Faca o cadastro do usuario após a selecao dos planos
+ * Solicite - nome - cpf - telefone - endereco 
+ * Mostre na tela as informacoes cadastrais
+ * Utilize o switch case para a seleção de categoria de planos 
+ *  1 - BASIC
+ *  2 - PLUS
+ *  3 - SILVER
+ *  4 - SILVER PLUS
+ *  5 - GOLD
+ *  9 - MAIS OPCOES
+ *  0 - SAIR
+ * Metodos de pagamento 
+ *  1 - DINHEIRO
+ *  2 - CARTAO
+ *  3 - PIX
+ *  4 - BOLETO
+ *  9 - MAIS OPCOES
+ *  0 - SAIR
+ */
