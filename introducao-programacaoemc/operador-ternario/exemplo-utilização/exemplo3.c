b72e2d84d87d9a347783fb634467c802d4c6031f
unknown <tvlisboa@hotmail.com>
2026-09-17 12:15:13 -0300

    exermplos-ternario-exemplo3

--- introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo3.c
+++ introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo3.c
@@ -20,19 +20,35 @@
     printf("************************* \n");
     printf("\n");
 
-    printf("* Informe dois numeros a seguir maiores que 0 \n");
-    printf("* Numero 1: ");
-    scanf("%d", &n1);
+    /* verifica se os numeros informados sao menores que zero */
+    do  {
 
-    printf("* Numero 2: ");
-    scanf("%d", &n2);
+        printf("* Informe dois numeros a seguir maiores que 0 \n");
+        printf("* Numero 1: ");
+        scanf("%d", &n1);
+
+        printf("* Numero 2: ");
+        scanf("%d", &n2);
+
+        if (n1 <0 || n2 <0) {
+            printf("Numeros informados são invalidos - tente novamente \n");
+        }
+
+    } while (n1<0 || n2 <0);
+    
+    n1>n2 ? printf("Numero informado %d e maior: \n" , n1):
+    n2>n1 ? printf("Numero informado %d e maior: \n" , n2): 
+            printf("Numeros informados - são iguais \n");
 
     /* teste de saida de dados */
+    printf("\n");
     printf("************************ \n");
     printf("Numero 1 informado: %d" , n1);
     printf("\n");
 
     printf("Numero 2 informado: %d" , n2);
+    printf("\n");
+    printf("************************ \n");
     printf("\n");
 
     system("pause");
