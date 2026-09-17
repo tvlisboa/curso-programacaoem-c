c001ffa5c3ffac0d23adbe0def06a52933b47f2e
unknown <tvlisboa@hotmail.com>
2026-09-17 10:03:15 -0300

    operador-ternario-exemplos

---
+++ introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo1.c
@@ -0,0 +1,37 @@
+/**
+ * Sintaxe e exemplo de utilização do operador ternário em c
+ * Solicite um numero inteiro ao usuario e verifique se é maior que 10
+ * Sendo maior - incremente 
+ * Sendo menor - decremente
+ * Utilize o operador ternario
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+    int n1;
+    int option;
+
+    printf("************************** \n");
+    printf("* OPERADOR TERNARIO EM C * \n");
+    printf("************************** \n");
+    printf("\n");
+
+        printf("* Numero 1 : ");
+        scanf("%d", &n1);
+
+        /* teste de saida de dados - antes da validacao */
+        printf("Numero informado pelo usuario: %d", n1);
+        printf("\n");
+
+        /* incrementa se for maior que 10 - decrementa se for menor que 10 */
+        n1 >= 10 ? n1++ : n1--;
+        printf("Novo valor do numero 1:  %d", n1);
+        printf("\n");
+
+    system("pause");
+    return 0;
+ }
+ 
---
+++ introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo2.c
@@ -0,0 +1,30 @@
+/**
+ * Sintaxe e exemplo de utilização do operador ternário em c
+ * Solicite um numero inteiro ao usuario e verifique se é POSITIVO ou NEGATIVO
+ * Utilize o operador ternario
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+    int n1;
+
+    printf("************************** \n");
+    printf("* OPERADOR TERNARIO EM C * \n");
+    printf("************************** \n");
+    printf("\n");
+
+    printf("VERIFICAR SE NUMERO E +- \n");
+    printf("* Informe um numero a seguir: \n");
+
+    n1 >=0 ? printf("Numero informado é positivo %d\n" , n1) :
+             printf("Numero informado é negativo %d\n" , n1) ;
+
+    /* se o numero for maior que zero - positivo , se for menor - negativo , caso contrario - neutro */
+        n1 >= 0 ? printf("Numero informado é positivo %d\n" , n1) : printf("Numero informado é negativo %d\n" , n1) : printf("Numero infomrado é neutro \n")
+    return 0;
+ }
+ 
+
