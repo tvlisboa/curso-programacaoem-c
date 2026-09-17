346dc4b4816f22c75fc7aebfff13f1fa1f3d4b28
unknown <tvlisboa@hotmail.com>
2026-09-17 13:03:07 -0300

    exemplo-ternarios-exemplo4&&exemplo5

--- introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo4.c
+++ introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo4.c
@@ -5,3 +5,29 @@
  * Numero negativo
  * Numero neutro
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+
+    int n1;
+
+    printf("*************************************** \n");
+    printf("* NUMERO POSITIVO - NEGATIVO - NEUTRO * \n");
+    printf("*************************************** \n");
+    printf("\n");
+
+
+    printf("Insira um numero inteiro a seguir: ");
+    scanf("%d", &n1);
+
+    n1>0 ? printf("Numero informado é positivo : %d\n" , n1) :
+    n1<0 ? printf("Numero informado é negativo : %d\n" , n1) :
+           printf("Numero informado é neutro : %d\n" , n1);
+
+    system("pause");
+    return 0;
+ }
+ 
--- introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo5.c
+++ introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo5.c
@@ -7,3 +7,44 @@
  * Media acima >= 5 - recuperacao
  * Media abaixo <5 - reprovado
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+
+    char nomeAluno[100];
+    double nota1, nota2;
+    double media;
+    
+    printf("********************************** \n");
+    printf("*   SITUAÇÃO APROVACAO ANUAL     * \n");
+    printf("********************************** \n");
+
+    printf("* Insira as informacoes a seguir * \n");
+    printf("* Nome do aluno: ");
+    scanf("%s", nomeAluno);
+
+    printf("* Notas do aluno * \n");
+    printf("* Nota 1: ");
+    scanf("%lf", &nota1);
+
+    printf("* Notas do aluno * \n");
+    printf("* Nota 2: ");
+    scanf("%lf", &nota2);
+
+    /* converter nome do usuario para uppercase */
+    /* fazer calculo da media do aluno */
+    /* implementar validacao - nao aceitar notas menores que 0 ou maiores que 10 */
+
+    /* teste de saida de dados */
+    printf("********************************** \n");
+    printf("Nome do aluno : %s\n", nomeAluno);
+    printf("Notas informadas \n");
+    printf("Nota 1 : %2.lf\n" , nota1);
+    printf("Nota 2 : %2.lf\n" , nota2);
+    system("pause");
+    return 0;
+ }
+ 
