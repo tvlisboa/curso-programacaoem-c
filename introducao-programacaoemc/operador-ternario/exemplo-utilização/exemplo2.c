85948eb3fab3899e0ce8e69103becd663bd2960e
unknown <tvlisboa@hotmail.com>
2026-09-18 12:34:29 -0300

    exemplo-ternario-exemplo2

--- introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo2.c
+++ introducao-programacaoemc/operador-ternario/exemplo-utilização/exemplo2.c
@@ -17,12 +17,18 @@
     printf("\n");
 
     printf("VERIFICAR SE NUMERO E POSITIVO - NEGATIVO - NEUTRO \n");
-    printf("* Informe um numero a seguir: \n");
+    printf("* Informe um numero a seguir: ");
     scanf("%d", &n1);
 
-    /* se o numero for maior que zero - positivo , se for menor - negativo , caso contrario - neutro */
+    /* 
+     * se o numero for maior que zero - positivo
+     * se for menor - negativo
+     * caso contrario - neutro  
+     */
+
     n1 >0 ? printf("Numero informado é positivo %d\n" , n1) :
-    n1 <0 ? printf("Numero informado é negativo %d\n" , n1) : printf("Numero informado é neutro %d\n" , n1);
+    n1 <0 ? printf("Numero informado é negativo %d\n" , n1) : 
+            printf("Numero informado é neutro %d\n" , n1);
 
     system("pause");
     return 0;
