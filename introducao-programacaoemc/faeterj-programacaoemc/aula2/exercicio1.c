cae212b42a5ab54038fb3b55610895d83cc2fae4
unknown <tvlisboa@hotmail.com>
2026-09-22 15:28:17 -0300

    exercicio1-faeterj-aula2

--- introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio1.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula2/exercicio1.c
@@ -1,28 +1,38 @@
 /* Solicite o nome do usuario
  * Seu peso e mostre na telas informacoes
  * Nome do usuario e o peso informado
+ * Idade do usuario e sua nacionalidade
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(){
 
-    char name[100];
+    char nome[100];
+    char nacionalidade[100];
     double peso;
+    int idade;
 
     printf("****************************** \n");
-    printf("* Insira seu nome a seguir : * \n");
-    scanf("%s", name);
+    printf("* Informe nome do usuario: * ");
+    scanf("%s", nome);
 
-    printf("Insira seu peso a seguir : ");
+    printf("Informe o peso do usuario: ");
     scanf("%lf", &peso);
+
+    printf("Informe a idade do usuario: ");
+    scanf("%d", &idade);
+
+    printf("Nacionalidade do usuario: ");
+    scanf("%s", nacionalidade);
 
     printf("********************************* \n");
     printf("* Dados informados pelo usuario *\n");
-    printf("* Nome: %s\n", name);
-    printf("* Peso: %.2lf\n", peso);
+    printf("* Nome do usuario: %s\n", nome);
+    printf("* Peso do usuario: %.2lf\n", peso);
+    printf("* Idade do usuario: %d\n", idade);
+    printf("* Nacionalidade do usuario: %s\n", nacionalidade);
     printf("\n* Fim do algoritmo! *\n");
     printf("********************************* \n");
     return 0;
