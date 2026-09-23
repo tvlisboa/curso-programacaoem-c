6b7d818171cf98fa331f4f594e2ccbf3293b0dbd
unknown <tvlisboa@hotmail.com>
2026-09-23 13:22:03 -0300

    revisao-exercicio6

--- introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/revisao/exercicio6.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/revisao/exercicio6.c
@@ -10,3 +10,34 @@
  * Nao aceite notas acima de 10 ou abaixo de 0
  * Mostre as informações na tela
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main (){
+
+    char nomeAluno[100];
+    char materia[50];
+
+    printf("*************************************** \n");
+    printf("* SISTEMA DE CADASTRO DE ALUNOS - SCA * \n");
+    printf("*************************************** \n");
+
+    printf("* Insira as informacoes do aluno a seguir * \n");
+    printf("* Nome do aluno: ");
+    scanf("%s", nomeAluno);
+
+    printf("* Insira os dados da materia e suas notas * \n");
+    printf("* Materia: ");
+    scanf("%s", materia);
+
+
+
+
+    /* teste de saida de dados */
+    printf("** Dados informados pelo professor ** \n");
+    printf("* Nome do aluno: %s\n" , nomeAluno);
+    printf("* Materia: %s\n" , materia);
+    system("pause");
+    return 0;
+ }
