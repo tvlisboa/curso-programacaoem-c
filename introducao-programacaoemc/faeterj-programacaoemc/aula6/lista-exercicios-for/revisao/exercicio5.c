8ebd43b7b7bd0cf0a7fce6fab136f72277b8c0d7
unknown <tvlisboa@hotmail.com>
2026-09-23 17:12:36 -0300

    revisao-exercicio5

--- introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/revisao/exercicio5.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/revisao/exercicio5.c
@@ -12,11 +12,13 @@
 
  #include <stdio.h>
  #include <stdlib.h>
+ #include <string.h>
 
  int main(int argc, char const *argv[])
  {
 
     int idadeCompetidor;
+    char mensagem[50];
 
     printf("************************************************* \n");
     printf("*             COMPETICAO CORPO E AGUA           * \n");
@@ -25,8 +27,22 @@
 
     printf("* IDADE DO COMPETIDOR: ");
     scanf("%d", idadeCompetidor);
-    
 
+    if(idadeCompetidor >=18){
+        strcpy(mensagem , "Sênior. \n");
+    }else if (idadeCompetidor >=14) {
+        strcpy(mensagem , "Juvenil B. \n");
+    }else if(idadeCompetidor >= 11){
+        strcpy(mensagem , "Juvenil A. \n");
+    }else if(idadeCompetidor >=7){
+        strcpy(mensagem , "Infantil C. \n");
+    }else if(idadeCompetidor >=5){
+        strcpy(mensagem , "Infantil B. \n");
+    }else{
+        strcpy(mensagem , "Infantil A. \n");
+    }
+    
+    
     /* teste de saida */
     printf("* Idade do competidor: %d\n" , idadeCompetidor);
     printf("* Categoria: %s\n");
