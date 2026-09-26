70800dd7786ecce1a7c7ef74eb7210865fd41627
tvlisboa <tvlisboa@hotmail.com>
2026-09-26 17:20:14 -0300

    lista-exercicio-for-exercicio5

--- introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio5.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio5.c
@@ -11,16 +11,23 @@
  {
 
     int numero1;
+    int soma;
+    double media;
     
     printf("************************* \n");
     printf("*      EXERCICIO 5      * \n");
     printf("*   INICIO DA CONTAGEM  * \n");
 
+    /* quantos numeros serao digitados pelo usuario */
     for (int i = 1; i <=5; i++) {
         printf("Informe um numero a seguir: ");
         scanf("%d", &numero1);
+
+        soma = soma + numero1;
     }
-    
+
+    media = (double)soma / 5;
+    printf("A media dos numeros informados foi: %.2lf\n" , media);
     system("pause");
     return 0;
  }
