f50a8db5bfb59cbbefb3e88f984f943b5bd2c9c8
tvlisboa <tvlisboa@hotmail.com>
2026-09-26 16:46:48 -0300

    lista-exercicio-for-exercicio3

--- introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio3.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio3.c
@@ -3,3 +3,26 @@
  * Solicite os dados de entrada - qual numero o usuario deseja contar
  * Mostre as informações na tela
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int numero1;
+
+ int main(){
+
+    printf("************************* \n");
+    printf("*      EXERCICIO 3      * \n");
+    printf("*   INICIO DA CONTAGEM  * \n");
+    printf("* Insira um numero a seguir: ");
+    scanf("%d", &numero1);
+
+    /* inica a contagem em 1 - enquanto ele for menor que 10 - incrementa 1 */
+    for(int i = 1 ; i<=10 ; i++){
+        printf("%d x %d = %d\n", numero1 , i , numero1 * i );
+    }
+
+    printf("*      FIM DA CONTAGEM      * \n");
+    system("pause");
+    return 0;
+ }
