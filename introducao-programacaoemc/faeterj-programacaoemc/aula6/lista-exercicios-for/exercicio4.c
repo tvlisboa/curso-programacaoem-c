1f94c68a52f11b31a0ce29b8bc9f58de596bf001
tvlisboa <tvlisboa@hotmail.com>
2026-09-26 16:51:24 -0300

    lista-exercicios-for-exercicio4

--- introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio4.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio4.c
@@ -1,3 +1,24 @@
 /**
  * Faca um algoritmo que leia de 1 até n - onde o usuário informar
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n1;
+
+    printf("************************* \n");
+    printf("*      EXERCICIO 4      * \n");
+    printf("*   INICIO DA CONTAGEM  * \n");
+    printf("* Insira um numero a seguir: ");
+    scanf("%d", &n1);
+
+
+
+    /* teste de saida de dados */
+    printf("Numero informado pelo usuario: %d\n" , n1);
+    system("pause");
+    return 0;
+ }
