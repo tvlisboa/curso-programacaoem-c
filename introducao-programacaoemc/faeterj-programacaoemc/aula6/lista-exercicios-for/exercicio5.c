637e0510204a9d9965908be01c079b7406d056c0
tvlisboa <tvlisboa@hotmail.com>
2026-09-26 17:11:56 -0300

    lista-exercicio-for-exercicio5

--- introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio5.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio5.c
@@ -3,3 +3,25 @@
  * Solicite ao usuario 5 numeros diferentes
  * Calcule sua media e mostre na tela as informações
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+
+    int numero1;
+    
+    printf("************************* \n");
+    printf("*      EXERCICIO 5      * \n");
+    printf("*   INICIO DA CONTAGEM  * \n");
+
+    for (int i = 1; i <=5; i++) {
+        printf("Informe um numero a seguir: ");
+        scanf("%d", &numero1);
+    }
+    
+    system("pause");
+    return 0;
+ }
+ 
