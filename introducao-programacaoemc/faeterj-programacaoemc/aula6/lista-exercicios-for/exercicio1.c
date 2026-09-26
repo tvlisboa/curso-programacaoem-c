caf886c29b6408bd80d9c3e9d1f3c4b7f5f83996
tvlisboa <tvlisboa@hotmail.com>
2026-09-26 16:10:42 -0300

    revisa-exercicios-for-exercio1&&2

--- introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio1.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio1.c
@@ -2,3 +2,23 @@
  * Crie uma lista iniciando em 0 e que conte ate 10
  * Utilizando a estrutura de repetição for
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+
+    printf("************************* \n");
+    printf("*      EXERCICIO 1      * \n");
+    printf("*   INICIO DA CONTAGEM  * \n");
+
+    for (int i = 0; i <=10; i++) {
+        printf("%d\n", i);
+    }
+
+    printf("*      FIM DA CONTAGEM      * \n");
+    system("pause");
+    return 0;
+ }
+ 
--- introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio2.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/exercicio2.c
@@ -2,3 +2,22 @@
  * Crie uma lista iniciando em 10 e va ate 0 
  * Utilizando a estrutura de repeticao for
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+    printf("************************* \n");
+    printf("*      EXERCICIO 2      * \n");
+    printf("*   INICIO DA CONTAGEM  * \n");
+
+    for (int i = 10 ; i >=0; i--){
+        printf("%d\n", i);
+    }
+
+    printf("*      FIM DA CONTAGEM      * \n");
+    system("pause");
+    return 0;
+ }
+ 
