b218d172b2a4c015ee75b3a53c9a3206fecaa222
tvlisboa <tvlisboa@hotmail.com>
2026-09-26 18:49:25 -0300

    lista-exercicio-for-exercicio8

--- introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/revisao/exercicio8.c
+++ introducao-programacaoemc/faeterj-programacaoemc/aula6/lista-exercicios-for/revisao/exercicio8.c
@@ -8,3 +8,15 @@
  * Quantos numeros sao negativos
  * Quantos numeros sao neutros
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+    
+
+    system("pause");
+    return 0;
+ }
+ 
